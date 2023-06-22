2fcc35fec89a6f99ba4e9c7978389770fabc038f
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-22 14:15:16 -0600

    Add 4 tests to calculator test suite

---
+++ js/calculator.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import calculator from './calculator';
+
+test('Add two numbers', () => {
+	expect(calculator.add(1, 2)).toBe(3);
+});
+test('Subtract two numbers', () => {
+	expect(calculator.subtract(11, 6)).toBe(5);
+});
+test('Multiply two numbers', () => {
+	expect(calculator.multiply(20, 6)).toBe(120);
+});
+test('Divide two numbers', () => {
+	expect(calculator.divide(200, 10)).toBe(20);
+});
