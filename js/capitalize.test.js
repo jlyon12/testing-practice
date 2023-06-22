20e6643a104c7bd329563c39c6671a246af750c8
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-22 13:03:12 -0600

    Pass capitalize test suite

--- js/capitalize.test.js
+++ js/capitalize.test.js
@@ -1,5 +1,22 @@
+/* eslint-disable no-undef */
 import capitalize from './capitalize';
 
-test('capitalizes the first letter of a string', () => {
-	expect(capitalize('this is a test.')).toEqual('This is a test');
+test('Capitalize first letter in string - single word', () => {
+	expect(capitalize('test')).toBe('Test');
+});
+
+test('Capitalize first letter in string - multiple words', () => {
+	expect(capitalize('this is a test')).toBe('This is a test');
+});
+
+test('Capitalize first letter in string - multiple words and numbers', () => {
+	expect(capitalize('this is a 2nd test')).toBe('This is a 2nd test');
+});
+test('Capitalize first letter in string - multiple words and punctuation', () => {
+	expect(capitalize('here! This is a 3rd test.')).toBe(
+		'Here! This is a 3rd test.'
+	);
+});
+test('Capitalize first letter in string - first letter is already capitalized', () => {
+	expect(capitalize('And a 4th test.')).toBe('And a 4th test.');
 });
