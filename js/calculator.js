893d653e2850e3907443079788e8d060f005ec25
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-22 14:16:45 -0600

    Pass calculator test suite

---
+++ js/calculator.js
@@ -0,0 +1,13 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const calculator = {
+	add,
+	subtract,
+	multiply,
+	divide,
+};
+calculator.add(3, 5);
+export default calculator;
