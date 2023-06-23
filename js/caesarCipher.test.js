985e44673ed390ac7f91b467bc5666ef909bf331
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-23 10:40:32 -0600

    Add Error tests for caesarCipher test suite

--- js/caesarCipher.test.js
+++ js/caesarCipher.test.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-undef */
 import caesarCipher from './caesarCipher';
 
+test('Throw Error if shift value is out of range', () => {
+	expect(() => caesarCipher('foo', 26)).toThrow(RangeError);
+	expect(() => caesarCipher('foo', 0)).toThrow(RangeError);
+});
+test('Throw TypeError if arg !== string', () => {
+	expect(() => caesarCipher(434, 5)).toThrow(TypeError);
+	expect(() => caesarCipher(true, 2)).toThrow(TypeError);
+});
 test('Encrypt a single word', () => {
 	expect(caesarCipher('phish', 5)).toBe('umnxm');
 });
 test('Letter wrapping', () => {
-	expect(caesarCipher('wxyz', 4)).toBe('abcd');
+	expect(caesarCipher('wxyza', 4)).toBe('abcde');
+	expect(caesarCipher('WXYZA', 4)).toBe('ABCDE');
 });
 test('Encrypt a multiple words', () => {
 	expect(caesarCipher('fly famous mockingbird', 3)).toBe(
